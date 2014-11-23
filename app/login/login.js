def286fb3796d368a6f9bb27ac23799cab735a2c
Dean Kerr <dean_k@live.co.uk>
2014-11-23 17:07:16 +0000

    Added basic login authentication

--- app/login/login.js
+++ app/login/login.js
@@ -2,6 +2,16 @@
 
 angular.module('myApp.login', [])
 
-.controller('LoginCtrl', ['AuthenticationService', function(authService) {
+.controller('LoginCtrl', ['$location', 'AuthenticationService', function($location, authService) {
 	console.log("Login controller instantiated", authService);
+
+	this.username = "";
+	this.password = "";
+
+	this.onLoginClick = function() {
+		if(this.username && this.password) {
+			authService.isLoggedIn = true;
+			$location.path("/home");
+		}
+	}
 }]);
